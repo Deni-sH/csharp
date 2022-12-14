a9ab4e5e580d8945f5baa389f2094e85583c98df
Denis <denis_hara@hotmail.com>
2022-12-14 19:22:54 -0300

    new

--- Program.cs
+++ Program.cs
@@ -1,8 +1,26 @@
 ﻿using amem.models;
 
-Pessoa pessoa1 = new Pessoa();
 
-pessoa1.Nome = "Denis";
-pessoa1.Idade = 26;
-pessoa1.Apresentar();
+int a = 10;
+int b = 20;
 
+int c = a + b;
+
+// c += 5; === c = c + 5;
+c += 5;
+
+
+
+
+
+
+
+// DateTime dataAtual = DateTime.Now.AddDays(5);
+// Console.WriteLine(dataAtual.ToString("dd/MM/yyyy HH:mm"));
+
+
+// Pessoa pessoa1 = new Pessoa();
+// pessoa1.Nome = "Denis";
+// pessoa1.Idade = 26;
+// pessoa1.Apresentar();
+ 
--- models/Pessoa.cs
+++ models/Pessoa.cs
@@ -7,7 +7,7 @@ namespace amem.models
 {
     public class Pessoa
     {
-        public string Nome { get; set; }
+        public string? Nome { get; set; }
         public int Idade { get; set; }
         public void Apresentar()
         {
