d9230fd0b159f19408fef5bb10ebfbec243dd404
Denis <denis_hara@hotmail.com>
2022-12-22 10:42:40 -0300

    sim

--- Program.cs
+++ Program.cs
@@ -1,6 +1,79 @@
 ﻿using amem.models;
 
+
+string opcao;
+bool exibirMenu = true;
+
+
+while(exibirMenu)
+
+{
+    Console.Clear();
+    Console.WriteLine("Digite a sua opção:");
+    Console.WriteLine("1 - Cadastrar cliente");
+    Console.WriteLine("2 - Buscar cliente");
+    Console.WriteLine("3 - Apagar cliente");
+    Console.WriteLine("4 - Encerrar");
+
+     opcao = Console.ReadLine();
+
+    
+    switch (opcao)
+
+
+    {
+        case "1":
+            Console.WriteLine("Cadastro de cliente");
+            break;
+
+        case "2":
+            Console.WriteLine("Busca de cliente");
+            break;
+
+        case "3":
+            Console.WriteLine("Apagar cliente");
+            break;
+
+        case "4":
+            Console.WriteLine("Encerrar");
+            exibirMenu = false;
+            //Environment.Exit(0);
+            break;
+
+        default:
+            Console.WriteLine("Opção invalida");
+            break;
+
+    }
+}
+Console.WriteLine("O programa se encerrou");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // ----------------------------------------------------------------------------------------
+
 
 // exercicio 1
 // using System.IO;
@@ -35,40 +108,40 @@
 // ---------------------------------------------------------------------------
 
 
-while (true)
-{
-    Console.WriteLine("Digite a sua opção:");
-    Console.WriteLine("1 - Cadastrar cliente");
-    Console.WriteLine("2 - Buscar cliente");
-    Console.WriteLine("3 - Apagar cliente");
-    Console.WriteLine("4 - Encerrar");
+// while (true)
+// {
+//     Console.WriteLine("Digite a sua opção:");
+//     Console.WriteLine("1 - Cadastrar cliente");
+//     Console.WriteLine("2 - Buscar cliente");
+//     Console.WriteLine("3 - Apagar cliente");
+//     Console.WriteLine("4 - Encerrar");
 
-    opcao = Console.ReadLine();
+//     opcao = Console.ReadLine();
 
-    switch (opcao)
-    {
-        case "1":
-            Console.WriteLine("Cadastro de cliente");
-            break;
+//     switch (opcao)
+//     {
+//         case "1":
+//             Console.WriteLine("Cadastro de cliente");
+//             break;
 
-        case "2":
-            Console.WriteLine("Busca de cliente");
-            break;
+//         case "2":
+//             Console.WriteLine("Busca de cliente");
+//             break;
 
-        case "3":
-            Console.WriteLine("Apagar cliente");
-            break;
-
-
-        case "4":
-            Console.WriteLine("Encerrar");
-            Environment.Exit(0);
-            break;
-            
+//         case "3":
+//             Console.WriteLine("Apagar cliente");
+//             break;
 
 
-    }
-}
+//         case "4":
+//             Console.WriteLine("Encerrar");
+//             Environment.Exit(0);
+//             break;
+
+
+
+//     }
+// }
 
 
 
